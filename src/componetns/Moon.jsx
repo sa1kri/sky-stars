c45c307c713e583bcdce3d380c9a7342ea732cbf
sa1kri <92201506+sa1kri@users.noreply.github.com>
2022-10-24 20:48:45 +0500

    Add files via upload

--- src/componetns/Moon.jsx
+++ src/componetns/Moon.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Sky.css';
+import { ReactComponent as IconMoon } from '../icons/moon.svg'; 
 
 function Moon() {
   return (
     <div className="Moon">
-      Луна
+      < IconMoon height={72} width= {72} fill={'#fdd835'}/>
     </div>
   );
 }
--- src/componetns/Star.jsx
+++ src/componetns/Star.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Sky.css';
 
-function Star() {
+function Star(props) {
   return (
-    <div className="Star">
-      Звезда
+    <div 
+    className="Star__wrapper"
+    style={{
+      left: props.star.left,
+      top: props.star.top,
+    }}
+    >
+      <div className="Star"></div>
     </div>
   );
 }
